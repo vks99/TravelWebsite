077224f5d2b45be5c2c3554add409bf0d9a314e3
Sethu Madhava Naidu Lankipalli <sethumadhav202@gmail.com>
2023-02-08 11:58:04 -0500

    Updated Footer

--- travel-client/src/components/footer.tsx
+++ travel-client/src/components/footer.tsx
@@ -6,36 +6,35 @@ const Footer=()=>{
             <div className='container-fluid bg-secondary'>
                 <div className='row'>
                   <div className='col'>
-                      <h2>Address</h2>
+                      <h2>Student 1</h2>
                       <ul className='list-unstyled'>
-                          <li>#205 Humber College</li>
-                          <li> Etobicocke</li>
-                          <li>Canada</li>
+                          <li>Vikas Uppala</li>
+                          <li> N01486527</li>
+                          <li>vikasgupta99@gmail.com</li>
                       </ul>
                   </div>
                   <div className="col">
-                    <h2>Popular Courses</h2>
+                    <h2>Student 2</h2>
                     <ul className='list-unstyled'>
-                       <li>Information Technology Solutions</li>
-                       <li>Project Management</li>
-                       <li>Enterprise Software Development</li>
+                       <li>Sarada Maddipatla</li>
+                       <li>N01486876</li>
+                       <li>saradamaddipatla364@gmail.com</li>
                     </ul>
                   </div>
                   <div className="col">
-                    <h2>Programs</h2>
+                    <h2>Student 3</h2>
                     <ul className='list-unstyled'>
-                       <li>Full-Time Programs</li>
-                       <li>Part-Time Programs</li>
-                       <li>Learning</li>
-                       <li>Continuous Professional</li>
+                       <li>Su Zhang</li>
+                       <li>N01572673</li>
+                       <li>suzhang@gmail.com</li>
                     </ul>
                   </div>
                   <div className="col">
-                    <h2>Popular Courses</h2>
+                    <h2>Student 4</h2>
                     <ul className='list-unstyled'>
-                       <li>Information Technology Solutions</li>
-                       <li>Project Management</li>
-                       <li>Enterprise Software Development</li>
+                       <li>Sethu Madhava Naidu Lankipalli</li>
+                       <li>N01445828</li>
+                       <li>sethumadhav202@gmail.com</li>
                     </ul>
                   </div>                
                 </div>
