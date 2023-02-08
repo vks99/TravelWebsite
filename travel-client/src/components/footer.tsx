34f66f992f59dc1f125cce4d7b95f97ed215c823
SaradaChowdary <saradamaddipatla364@gmail.com>
2023-02-08 00:02:33 -0500

    added navbar and footer

--- travel-client/src/components/footer.tsx
+++ travel-client/src/components/footer.tsx
@@ -1,10 +1,52 @@
 import { NavLink } from 'react-router-dom';
 
 const Footer=()=>{
-    return(
-        <div>
-            Footer
-        </div>
+    return( 
+            <div className='main-footer' >
+            <div className='container-fluid bg-secondary'>
+                <div className='row'>
+                  <div className='col'>
+                      <h2>Address</h2>
+                      <ul className='list-unstyled'>
+                          <li>#205 Humber College</li>
+                          <li> Etobicocke</li>
+                          <li>Canada</li>
+                      </ul>
+                  </div>
+                  <div className="col">
+                    <h2>Popular Courses</h2>
+                    <ul className='list-unstyled'>
+                       <li>Information Technology Solutions</li>
+                       <li>Project Management</li>
+                       <li>Enterprise Software Development</li>
+                    </ul>
+                  </div>
+                  <div className="col">
+                    <h2>Programs</h2>
+                    <ul className='list-unstyled'>
+                       <li>Full-Time Programs</li>
+                       <li>Part-Time Programs</li>
+                       <li>Learning</li>
+                       <li>Continuous Professional</li>
+                    </ul>
+                  </div>
+                  <div className="col">
+                    <h2>Popular Courses</h2>
+                    <ul className='list-unstyled'>
+                       <li>Information Technology Solutions</li>
+                       <li>Project Management</li>
+                       <li>Enterprise Software Development</li>
+                    </ul>
+                  </div>                
+                </div>
+                <div className="row">
+                 <p className='col-sm'>
+                    &copy;{new Date().getFullYear()} Terms Of Service | Privacy
+                 </p>   
+                </div>
+            </div>
+            </div>
+
     )
 }
 
--- travel-client/src/pages/Navigation.tsx
+++ travel-client/src/pages/Navigation.tsx
@@ -1,10 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink,  } from 'react-router-dom';
+import { Navbar, Nav,Container } from 'react-bootstrap';
 
 const Navigation=()=>{
     return(
-        <div>
-            Navigation
-        </div>
+      <div className='main-nav'>
+      {/* <Navbar bg="success" expand="lg">
+      <Navbar.Brand href="/">My Website</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto ">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Destination">Destination</NavLink>
+          <NavLink to="/Blogs">Blogs </NavLink>
+          <NavLink to="/History">History </NavLink>
+          <NavLink to="/Contactus">Contactus</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar> */}
+
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">TravelCation</Navbar.Brand>
+                    <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/Destination">Destination</Nav.Link>
+                    <Nav.Link href="/Blogs">Blogs</Nav.Link>
+                    <Nav.Link href="/History">History</Nav.Link>
+                    <Nav.Link href="/Contactus">Contactus</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    </div>
     )
 }
 
