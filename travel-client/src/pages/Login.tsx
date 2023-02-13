150b15d1b088002bc75c0b8c332f8239079d4c63
SaradaChowdary <saradamaddipatla364@gmail.com>
2023-02-13 14:53:13 -0500

    updated input tag in login signup

--- travel-client/src/pages/Login.tsx
+++ travel-client/src/pages/Login.tsx
@@ -4,12 +4,12 @@ const Login=()=>{
     return(
         <div>
     <form method="POST" action="/login">
-	<input type="text" name="email" placeholder="email">
-	<input type="text" name="password" placeholder="password">
-	<input type="submit">
+	<input type="text" name="email" placeholder="email"/>
+	<input type="text" name="password" placeholder="password"/>
+	<input type="submit"/>
 	</form>
         </div>
     )
-}
+};
 
 export default Login;
--- travel-client/src/pages/Signup.tsx
+++ travel-client/src/pages/Signup.tsx
@@ -4,11 +4,11 @@ const Signup=()=>{
     return(
         <div>
     <form method="POST" action="/register">
-	<input type="text" name="first_name" placeholder="first_name">
-	<input type="text" name="last_name" placeholder="last_name">
-	<input type="text" name="email" placeholder="email">
-	<input type="text" name="password" placeholder="password">
-	<input type="submit">
+	<input type="text" name="first_name" placeholder="first_name" />
+	<input type="text" name="last_name" placeholder="last_name" />
+	<input type="text" name="email" placeholder="email" />
+	<input type="text" name="password" placeholder="password"/>
+	<input type="submit"/>
 	</form>
         </div>
     )
